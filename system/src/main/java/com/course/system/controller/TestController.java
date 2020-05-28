c05a42983b146394e9a2c4a6cb33df8fd37bcce1
yeshouyi-pd <172793208@qq.com>
2020-05-28 12:12:43 +0800

    1 添加代码生产工具 2 添加server 模块

--- system/src/main/java/com/course/system/controller/TestController.java
+++ system/src/main/java/com/course/system/controller/TestController.java
@@ -1,14 +1,25 @@
 package com.course.system.controller;
 
+import com.course.server.domain.Test;
+import com.course.server.domain.TestExample;
+import com.course.server.service.TestServer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class TestController {
 
+    @Autowired
+    private TestServer testServer;
+
     @RequestMapping("/test")
-    public  String test(){
-        return  "success";
+    public List<Test> test() {
+        TestExample example = new TestExample();
+        example.createCriteria().andIdEqualTo("1");
+        return testServer.getInfo(example);
 
     }
 }
