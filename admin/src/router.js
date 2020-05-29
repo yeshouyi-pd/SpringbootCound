571e88a7438698692fccaea2a5186ec254021c00
yeshouyi-pd <172793208@qq.com>
2020-05-29 13:58:55 +0800

    1 增加admin页面 2 login页面跳转 3 mounted 初始化方法

--- admin/src/router.js
+++ admin/src/router.js
@@ -1,6 +1,7 @@
 import  Vue from 'vue'
 import Router from 'vue-router'
 import Login from  './views/login.vue'
+import Admin from  './views/admin.vue'
 
 Vue.use(Router);
 
@@ -13,6 +14,9 @@ export default new Router({
     },{
         path: '/login',
         component:Login
+    },{
+        path: '/admin',
+        component:Admin
     }]
 
 })
--- admin/src/views/login.vue
+++ admin/src/views/login.vue
@@ -48,7 +48,9 @@
                                                         <span class="lbl">记住我</span>
                                                     </label>
 
-                                                    <button type="button" class="width-35 pull-right btn btn-sm btn-primary">
+                                                    <button type="button"
+                                                            class="width-35 pull-right btn btn-sm btn-primary"
+                                                             v-on:click="login()" >
                                                         <i class="ace-icon fa fa-key"></i>
                                                         <span class="bigger-110">Login</span>
                                                     </button>
@@ -146,10 +148,20 @@
 </template>
 
 <script>
-    $('body').attr('class', 'login-layout light-login');
 
     export default {
-        name: 'App',
+        name:'login',
+        mounted:function(){//mounted初始化方法
+            $('body').removeClass('on-skin');
+            $('body').attr('class', 'login-layout light-login');
+
+
+        },
+        methods:{
+            login(){//push跳转到某一个地址
+                this.$router.push("/admin")
+            }
+        }
 
     }
 </script>
