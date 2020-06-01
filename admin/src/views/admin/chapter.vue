a0ce04f123bccc56f349e13ae2c6bd91c347bb8b
yeshouyi-pd <172793208@qq.com>
2020-06-01 13:00:35 +0800

    1 添加刷新功能

--- admin/src/views/admin/chapter.vue
+++ admin/src/views/admin/chapter.vue
@@ -1,4 +1,11 @@
 <template>
+    <div>
+        <p>
+        <button @click="list()" class="btn btn-white btn-default btn-round">
+            <i class="ace-icon fa fa-refresh "></i>
+            刷新
+        </button>
+        </p>
         <table id="simple-table" class="table  table-bordered table-hover">
             <thead>
             <tr>
@@ -76,6 +83,7 @@
 
             </tbody>
         </table>
+    </div>
 </template>
 <script>
     export default {
