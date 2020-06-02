6bd57c45f302c131401127b85c6580dc0bf8a227
yeshouyi-pd <172793208@qq.com>
2020-06-02 13:38:19 +0800

    1增加ggeneratar模块 2 增加模板文件

--- admin/src/main.js
+++ admin/src/main.js
@@ -6,6 +6,19 @@ import axios from "axios";
 Vue.config.productionTip = false
 Vue.prototype.$ajax = axios;//添加内置熟悉 Vue.prototype.xx 全局变量
 
+
+/**
+ * axios拦截器
+ */
+axios.interceptors.request.use(function (config) {
+    console.log("请求：", config);
+    return config;
+}, error => {});
+axios.interceptors.response.use(function (response) {
+    console.log("返回结果：", response);
+    return response;
+}, error => {});
+
 new Vue({
     router,//需要加载router路由配置
     render: h => h(App),
