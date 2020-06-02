29438b9e1e794051b954dbc18a0356b982eed5c4
yeshouyi-pd <172793208@qq.com>
2020-06-02 13:59:21 +0800

    1增加ggeneratar模块 2 增加模板文件

--- db/all.sql
+++ db/all.sql
@@ -22,3 +22,21 @@ insert into `course`.`chapter` (`id`, `course_id`, `name`) values ('16', '16', '
 insert into `course`.`chapter` (`id`, `course_id`, `name`) values ('17', '17', '语文');
 insert into `course`.`chapter` (`id`, `course_id`, `name`) values ('18', '18', '数学 ');
 insert into `course`.`chapter` (`id`, `course_id`, `name`) values ('19', '19', '英语');
+---------------
+CREATE TABLE `course`.`section`(
+                                   `id` CHAR(8) NOT NULL COMMENT 'ID',
+                                   `title` VARCHAR(50) COMMENT '标题',
+                                   `course_id` CHAR(8) COMMENT '课程|course.id',
+                                   `chapter_id` CHAR(8) COMMENT '大章|chapter.id',
+                                   `video` VARCHAR(200) COMMENT '视频',
+                                   `time` INT COMMENT '时长|单位秒',
+                                   `charge` CHAR(1) COMMENT '收费|C 收费; F免费',
+                                   `sort` INT COMMENT '顺序',
+                                   `created_at` DATETIME COMMENT '创建时间',
+                                   `updated_at` DATETIME COMMENT '修改时间',
+                                   PRIMARY KEY (`id`)
+) CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+INSERT INTO section (`id`, `title`, `course_id`, `chapter_id`, `video`, `time`, `charge`, `sort`, `created_at`, `updated_at` )
+VALUES ( '00000001','测试小节01','00000001','000000', '',500, 'F', 1, now(), now());
