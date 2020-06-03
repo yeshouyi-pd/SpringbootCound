5db4b90ce3a447dc047a1ebde0d305f51abd9867
yeshouyi-pd <172793208@qq.com>
2020-06-03 10:25:12 +0800

    1 新增课程 模块

--- admin/src/views/admin.vue
+++ admin/src/views/admin.vue
@@ -414,9 +414,17 @@
                               <b class="arrow"></b>
                           </li>
                           <li class="active" id = "business-section-sidebar">
-                              <router-link to="/business/section">
+                          <router-link to="/business/section">
+                              <i class="menu-icon fa fa-caret-right"></i>
+                              小节管理
+                          </router-link>
+
+                          <b class="arrow"></b>
+                      </li>
+                          <li class="active" id = "business-course-sidebar">
+                              <router-link to="/business/course">
                                   <i class="menu-icon fa fa-caret-right"></i>
-                                  小节管理
+                                  课程管理
                               </router-link>
 
                               <b class="arrow"></b>
