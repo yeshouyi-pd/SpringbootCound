d6e8176f11bcc391f1376231ee73989ce3b35e42
yeshouyi-pd <172793208@qq.com>
2020-06-15 15:12:49 +0800

    1优化讲师显示

--- admin/src/views/admin/teacher.vue
+++ admin/src/views/admin/teacher.vue
@@ -13,43 +13,50 @@
     </p>
 
     <pagination ref="pagination" v-bind:list="list" v-bind:itemCount="8"></pagination>
+      <div class="row">
+      <div   class="col-md-3 center" v-for="teacher in teachers">
+      <div>
+			<span class="profile-picture"  >
+                <img v-bind:title="teacher.intro"  v-show="!teacher.image" class="editable img-responsive editable-click editable-empty" alt="亚历克斯的头像"  src="/static/image/profile-pic.jpg" />
+                <img  v-bind:title="teacher.intro" v-show="teacher.image" class="editable img-responsive editable-click editable-empty" alt="亚历克斯的头像"  v-bind:src="teacher.image" />
+              </span>
 
-    <table id="simple-table" class="table  table-bordered table-hover">
-      <thead>
-      <tr>
-                    <th>id</th>
-            <th>姓名</th>
-            <th>昵称</th>
-            <th>头像</th>
-            <th>职位</th>
-            <th>座右铭</th>
-            <th>简介</th>
-        <th>操作</th>
-      </tr>
-      </thead>
+          <div class="space-4"></div>
 
-      <tbody>
-      <tr v-for="teacher in teachers">
-              <td>{{teacher.id}}</td>
-              <td>{{teacher.name}}</td>
-              <td>{{teacher.nickname}}</td>
-              <td>{{teacher.image}}</td>
-              <td>{{teacher.position}}</td>
-              <td>{{teacher.motto}}</td>
-              <td>{{teacher.intro}}</td>
-        <td>
-          <div class="hidden-sm hidden-xs btn-group">
-            <button v-on:click="edit(teacher)" class="btn btn-xs btn-info">
-              <i class="ace-icon fa fa-pencil bigger-120"></i>
-            </button>
-            <button v-on:click="del(teacher.id)" class="btn btn-xs btn-danger">
-              <i class="ace-icon fa fa-trash-o bigger-120"></i>
-            </button>
+          <div class="width-85 label label-info label-xlg arrowed-in arrowed-in-right">
+              <div class="inline position-relative">
+                  <a href="#" class="user-title-label dropdown-toggle" data-toggle="dropdown">
+                      <i class="ace-icon fa fa-circle light-green"></i>
+                      &nbsp;
+                      <span class="white">{{teacher.position}}</span>
+                  </a>
+              </div>
           </div>
-        </td>
-      </tr>
-      </tbody>
-    </table>
+      </div>
+
+      <div class="space-6"></div>
+
+      <div class="text-center">
+          <a href="javascript:;" class="text-info bigger-110" v-bind:title="teacher.motto">
+              <i class="ace-icon fa fa-user"></i>
+              {{teacher.name}}【{{teacher.nickname}}】
+          </a>
+          <div class="space-4"></div>
+
+          <div class="profile-social-links align-center">
+                  <button v-on:click="edit(teacher)" class="btn btn-xs btn-info">
+                      <i class="ace-icon fa fa-pencil bigger-120"></i>
+                  </button>
+              &nbsp; &nbsp;
+                  <button v-on:click="del(teacher.id)" class="btn btn-xs btn-danger">
+                      <i class="ace-icon fa fa-trash-o bigger-120"></i>
+                  </button>
+          </div>
+          <div class="hr hr16 dotted"></div>
+      </div>
+  </div>
+      </div>
+
 
     <div id="form-modal" class="modal fade" tabindex="-1" role="dialog">
       <div class="modal-dialog" role="document">
@@ -93,7 +100,7 @@
                     <div class="form-group">
                       <label class="col-sm-2 control-label">简介</label>
                       <div class="col-sm-10">
-                        <input v-model="teacher.intro" class="form-control">
+                          <textarea v-model="teacher.intro" class="form-control" rows="5"></textarea>
                       </div>
                     </div>
             </form>
