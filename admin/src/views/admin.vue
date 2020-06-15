1cae5d34ccdc5a91899a4a87491e99bf46750f52
yeshouyi-pd <172793208@qq.com>
2020-06-15 13:31:24 +0800

    1添加讲师菜单

--- admin/src/views/admin.vue
+++ admin/src/views/admin.vue
@@ -414,9 +414,17 @@
                               <b class="arrow"></b>
                           </li>
                           <li class="active" id = "business-course-sidebar">
-                              <router-link to="/business/course">
+                          <router-link to="/business/course">
+                              <i class="menu-icon fa fa-caret-right"></i>
+                              课程管理
+                          </router-link>
+
+                          <b class="arrow"></b>
+                      </li>
+                          <li class="active" id = "business-teacher-sidebar">
+                              <router-link to="/business/teacher">
                                   <i class="menu-icon fa fa-caret-right"></i>
-                                  课程管理
+                                  讲师管理
                               </router-link>
 
                               <b class="arrow"></b>
