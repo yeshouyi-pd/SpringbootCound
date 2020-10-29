d4742dabdbf75bb42850de19411ea8cb62b70a73
yeshouyi-pd <172793208@qq.com>
2020-10-29 15:01:43 +0800

    权限管理  登录时加载用户资源

--- server/src/main/java/com/course/server/service/UserService.java
+++ server/src/main/java/com/course/server/service/UserService.java
@@ -1,13 +1,16 @@
 package com.course.server.service;
 
+import com.alibaba.fastjson.JSON;
 import com.course.server.domain.User;
 import com.course.server.domain.UserExample;
 import com.course.server.dto.LoginUserDto;
+import com.course.server.dto.ResourceDto;
 import com.course.server.dto.UserDto;
 import com.course.server.dto.PageDto;
 import com.course.server.exception.BusinessException;
 import com.course.server.exception.BusinessExceptionCode;
 import com.course.server.mapper.UserMapper;
+import com.course.server.mapper.my.MyUserMapper;
 import com.course.server.util.CopyUtil;
 import com.course.server.util.UuidUtil;
 import com.github.pagehelper.PageHelper;
@@ -19,6 +22,7 @@ import org.springframework.util.CollectionUtils;
 import org.springframework.util.StringUtils;
 
 import javax.annotation.Resource;
+import java.util.HashSet;
 import java.util.List;
 
 @Service
@@ -26,6 +30,10 @@ public class UserService {
 
 @Resource
 private UserMapper userMapper;
+
+    @Resource
+    private MyUserMapper myUserMapper;
+
     private static final Logger LOG = LoggerFactory.getLogger(UserService.class);
 /**
 * 列表查询
@@ -105,7 +113,8 @@ List<UserDto> userDtoList = CopyUtil.copyList(userList, UserDto.class);
                 if (user.getPassword().equals(userDto.getPassword())) {
                     // 登录成功
                     LoginUserDto loginUserDto = CopyUtil.copy(user, LoginUserDto.class);
-
+                    // 为登录用户读取权限
+                    setAuth(loginUserDto ,user.getRode());
                     return loginUserDto;
                 } else {
                     LOG.info("密码不对, 输入密码：{}, 数据库密码：{}", userDto.getPassword(), user.getPassword());
@@ -113,5 +122,28 @@ List<UserDto> userDtoList = CopyUtil.copyList(userList, UserDto.class);
                 }
             }
         }
+    }
+
+    /**
+     * 为登录用户读取权限
+     */
+    private void setAuth(LoginUserDto loginUserDto,String rode) {
+        List<ResourceDto> resourceDtoList = myUserMapper.findResources(rode);
+        loginUserDto.setResources(resourceDtoList);
+
+        // 整理所有有权限的请求，用于接口拦截
+        HashSet<String> requestSet = new HashSet<>();
+        if (!CollectionUtils.isEmpty(resourceDtoList)) {
+            for (int i = 0, l = resourceDtoList.size(); i < l; i++) {
+                ResourceDto resourceDto = resourceDtoList.get(i);
+                String arrayString = resourceDto.getRequest();
+                List<String> requestList = JSON.parseArray(arrayString, String.class);
+                if (!CollectionUtils.isEmpty(requestList)) {
+                    requestSet.addAll(requestList);
+                }
+            }
+        }
+        LOG.info("有权限的请求：{}", requestSet);
+        loginUserDto.setRequests(requestSet);
     }
 }
